603d518b62fe0d8a418f857fa9447608d6f6efd1
Lukas Lehmann <lukas.lehmann97@gmail.com>
2021-06-05 16:02:22 +0200

    fix mongo.py

--- src/main/mongo.py
+++ src/main/mongo.py
@@ -3,7 +3,10 @@ import os
 import pymongo
 from pymongo.database import Database
 
-MONGO_URI = os.environ['MONGO_URI'] or 'localhost:27017'
+try:
+    MONGO_URI = os.environ['MONGO_URI']
+except KeyError:
+    MONGO_URI = 'localhost:27017'
 
 client = pymongo.MongoClient(MONGO_URI)
 db: Database = client.get_database('KomootAnalyzer')
